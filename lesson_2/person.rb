997b7f73ec08741bd017576be46c6bd9c8e442ce
menachemkorf <menachemkorf@gmail.com>
2016-06-14 12:48:59 +0000

    lasson 2 exercises(1)

---
+++ lesson_2/person.rb
@@ -0,0 +1,41 @@
+class Person
+  attr_accessor :first_name, :last_name
+
+  def initialize(full_name)
+    parse_full_name(full_name)
+  end
+
+  def name=(full_name)
+    parse_full_name(full_name)
+  end
+
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+
+  def to_s
+    name
+  end
+
+  private
+
+  def parse_full_name(full_name)
+    parts = full_name.split
+    @first_name = parts.first
+    @last_name = parts.size > 1 ? parts.last : ''
+  end
+end
+
+bob = Person.new('Robert Smith')
+rob = Person.new('Robert Smith')
+# p bob.name                  # => 'Robert'
+# p bob.first_name            # => 'Robert'
+# p bob.last_name             # => ''
+# bob.last_name = 'Smith'
+# p bob.name                  # => 'Robert Smith'
+
+# bob.name = "John Adams"
+# p bob.first_name            # => 'John'
+# p bob.last_name             # => 'Adams'
+
+puts bob

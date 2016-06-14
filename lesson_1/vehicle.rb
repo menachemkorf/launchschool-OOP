d79753886620d19491c00d7e0f66b95fff0bcb03
menachemkorf <menachemkorf@gmail.com>
2016-06-14 11:43:51 +0000

    oo book - inheritance

--- lesson_1/vehicle.rb
+++ lesson_1/vehicle.rb
@@ -1,13 +1,21 @@
-class MyCar
+module Towable
+  def can_tow?(pounds)
+    pounds < 2000 ? true : false
+  end
+end
 
+class Vehicle
   attr_accessor :color
   attr_reader :year
+
+  @@number_of_vehicles = 0
 
   def initialize(year, model, color)
     @year = year
     @model = model
     @color = color
     @current_speed = 0
+    @@number_of_vehicles += 1
   end
 
   def speed_up(number)
@@ -34,9 +42,28 @@ class MyCar
     puts "You new #{color} paint job looks great!"
   end
 
+  def age
+    puts "Your car is #{years_old} years old."
+  end
+
   def self.gas_mileage(gallons, miles)
     puts "#{miles / gallons} miles per gallon of gas."
   end
+
+  def self.number_of_vehicles
+    puts "#{@@number_of_vehicles} vehicles were instantiated"
+  end
+
+  private
+    def years_old
+      Time.now.year - @year
+    end
+
+end
+
+class MyCar < Vehicle
+
+  NUMBER_OF_DOORS = 4
 
   def to_s
     puts "MyCar is a #{@color}, #{@year}, #{@model}!"
@@ -44,19 +71,25 @@ class MyCar
 
 end
 
+class MyTruck < Vehicle
+
+  include Towable
+
+  NUMBER_OF_DOORS = 2
+
+  def to_s
+    puts "MyTruck is a #{@color}, #{@year}, #{@model}!"
+  end
+
+end
+
 lumina = MyCar.new(1997, 'chevy lumina', 'white')
-# lumina.speed_up(20)
-# lumina.current_speed
-# lumina.speed_up(20)
-# lumina.current_speed
-# lumina.brake(20)
-# lumina.current_speed
-# lumina.brake(20)
-# lumina.current_speed
-# lumina.shut_down
-# lumina.current_speed
-# lumina.spray_paint('black')
-# puts lumina.color
-# puts lumina.year
-# MyCar.gas_mileage(13, 351)
-# lumina.to_s
+colorado = MyTruck.new(2015, 'chevy colorado', 'red')
+# Vehicle.number_of_vehicles
+# puts MyCar.ancestors
+# puts ""
+# puts MyTruck.ancestors
+# puts ""
+# puts Vehicle.ancestors
+# lumina.age
+# colorado.age

68a17d2c3a1990b0143c06fec2afba2919bfa2a5
menachemkorf <menachemkorf@gmail.com>
2016-07-20 01:36:42 +0000

    initialized deck

--- lesson_5/twenty_one.rb
+++ lesson_5/twenty_one.rb
@@ -1,13 +1,59 @@
 class Deck
 
+  SUITS = ['H', 'D', 'S', 'C']
+  FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  SETS = SUITS.product(FACES)
+
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    SETS.each { |set| @cards << Card.new(*set) }
+  end
+
+  def deal
+
+  end
+
+  def to_s
+    cards
+  end
 end
 
 class Card
+  attr_accessor :suit, :face, :value
 
+  def initialize(suit, face)
+    @suit = suit
+    @face = face
+    @value = { suit: suit, face: face }
+  end
+
+  def to_s
+    value.to_s
+  end
 end
 
 class Participant
+  def initialize
+    # cards, name
+  end
 
+  def hit
+
+  end
+
+  def stay
+
+  end
+
+  def busted?
+
+  end
+
+  def total
+
+  end
 end
 
 class Player < Participant
@@ -19,9 +65,22 @@ class Dealer < Participant
 end
 
 class Game
-  def start
-    puts "Welcome to Twenty-One!"
+  def initialize
+    @deck = Deck.new
+    puts @deck.cards.shuffle
   end
+
+  def start
+
+    # deal_cards
+    # show_initial_cards
+    # player_turn
+    # dealer_turn
+    # show_result
+  end
+
+  private
+
 end
 
 Game.new.start
